4d8c92e917a6215dd90b63b21f917bbf6d0e575c
Manideep159 <105004283+Manideep159@users.noreply.github.com>
2026-06-11 11:12:27 +0530

    Frontend ready for production deployment

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  // baseURL: "http://localhost:9090"
 });
 
 API.interceptors.request.use((config) => {
